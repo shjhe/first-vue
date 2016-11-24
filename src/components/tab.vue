894de353110e67514d6bd92d22e319dade7634d9
shjhe <568675732@qq.com>
2016-11-25 00:30:04 +0800

    feature

---
+++ src/components/tab.vue
@@ -0,0 +1,28 @@
+<style>
+
+
+
+</style>
+<template>
+  <div class="tab-content">
+    <span v-if=""></span>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import hello from './components/hello'
+  exports.default{
+    data () {
+      return {
+        data:''
+      }
+    },
+    props:[],
+    methods:{
+
+    },
+    created () {
+      
+    }
+  }
+</script>

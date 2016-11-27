34fb475a74a3b6f2146b90db54a032ce016f50fb
shjhe <568675732@qq.com>
2016-11-28 02:40:31 +0800

    update

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,10 @@
+<style lang="less" rel="stylessheet/less">
+
+</style>
+<template>
+  <div>I am goods</div>
+</template>
+
+<script>
+export default{};
+</script>
---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,10 @@
+<style lang="less" rel="stylessheet/less">
+
+</style>
+<template>
+  <div>I am ratings</div>
+</template>
+
+<script>
+export default{};
+</script>

7d69617567851301dfd93167ff399abedccd1170
shjhe <568675732@qq.com>
2016-11-30 08:22:43 +0800

    update

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,10 +1,195 @@
-<style lang="less" rel="stylessheet/less">
-
-</style>
 <template>
-  <div>I am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper" v-el:menu-wrapper>
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text border-1px">
+            <span class="icon" v-show="item.type>0" :class="classMap[item.type]"></span>
+            {{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper" v-el:foods-wrapper>
+      <ul>
+        <li class="food-list" v-for="item in goods">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item border-1px">
+              <div class="icon">
+                <img :src="food.icon" width="57px"/>
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-export default{};
+import BScroll from 'better-scroll';
+export default{
+  data() {
+    return {
+      goods: {}
+    };
+  },
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    this.$http.get('/api/goods').then(
+      (req) => {
+        req = req.body;
+        if (req.errno === 0) {
+          this.goods = req.data;
+          this.$nextTick(() => {
+            this._initScroll();
+          });
+        }
+      }
+    );
+  },
+  methods: {
+    _initScroll() {
+      this.menuScroll = new BScroll(this.$els.menuWrapper, {});
+      this.foodScroll = new BScroll(this.$els.foodsWrapper, {});
+    }
+  }
+};
 </script>
+<style lang="less" rel="stylessheet/less">
+  @import "../../common/less/mixin.less";
+  .goods{
+    display:flex;
+    position:absolute;
+    top:174px;
+    bottom:46px;
+    width:100%;
+    overflow:hidden;
+    .menu-wrapper{
+      flex:0 0 80px;
+      width:80px;//不写这个width在安卓下会有问题
+      background:#f3f5f7;
+      .menu-item{
+        display:table;
+        height:54px;
+        width:56px;
+        padding:0 12px;
+        line-height:14px;
+        .icon{
+          display:inline-block;
+          vertical-align:middle;
+          height:12px;
+          width:12px;
+          margin-right:2px;
+          background-size:12px 12px;
+          background-repeat:no-repeat;
+          &.decrease{
+            .bg-img('../../components/goods/decrease_3');
+          }
+          &.discount{
+            .bg-img('../../components/goods/discount_3');
+          }
+          &.guarantee{
+            .bg-img('../../components/goods/guarantee_3');
+          }
+          &.invoice{
+            .bg-img('../../components/goods/invoice_3');
+          }
+          &.special{
+            .bg-img('../../components/goods/special_3');
+          }
+        }
+        .text{
+          display:table-cell;
+          width:56px;
+          vertical-align:middle;
+          font-size:12px;
+          .border-1px(rgba(7,17,27,0.1));
+        }
+      }
+    }
+    .foods-wrapper{
+      flex:1;
+      .title{
+        padding-left:17px;
+        height:26px;
+        line-height:26px;
+        border-left:2px solid #d9dde1;
+        font-size:12px;
+        color:rgb(147,153,159);
+        background-color:#f3f5f7;
+      }
+      .food-item{
+        display:flex;
+        margin:18px;
+        padding-bottom:18px;
+        .border-1px(rgba(7,17,27,0.1));
+        &:last-child{
+          .border-none();
+          margin-bottom:0;
+        }
+        .icon{
+          flex:0 0 57px;
+          margin-right:10px;
+        }
+        .content{
+          flex:1;
+          .name{
+            margin:2px 0 8px;
+            height:14px;
+            line-height:14px;
+            font-size:14px;
+            color:rgb(7,17,27);
+          }
+          .desc,.extra{
+            line-height:10px;
+            font-size:10px;
+            color:rgb(147,153,159);
+          }
+          .desc{
+            line-height:12px;
+            margin-bottom:8px;
+          }
+          .extra{
+            &>.count{
+              margin-right:12px;
+            }
+          }
+          .price{
+            font-weight:700;
+            line-height:24px;
+            .now{
+              margin-right:8px;
+              font-size:14px;
+              color:rgb(240,20,20);
+            }
+            .old{
+              text-decoration:line-through;
+              font-size:10px;
+              color:rgb(147,153,159);
+            }
+          }
+        }
+      }
+    }
+
+  }
+</style>
